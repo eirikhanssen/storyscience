2feb19934613c3a3a49d83609b5c42a27f1599fd
Eirik Hanssen <eirikhanssen79@gmail.com>
2015-12-03 10:25:29 +0100

    page.surround.inc.php will include helper classes to generate markup that appears on any page

--- chapter/06/short.php
+++ chapter/06/short.php
@@ -1,9 +1,11 @@
 <?php 
 include '../../include/xslt2.inc.php';
+include '../../include/page.surround.inc.php';
 $xml ="../../locale/no/locale.xml";
 $xsl ="../../xsl/keys.xsl";
-
-//echo $xml;
-//echo $xsl;
+$bp = new BeginPage;
+echo $bp->display();
 echo xslt2($xml,$xsl);
 ?>
+   </body>
+</html>
---
+++ include/page.surround.inc.php
@@ -0,0 +1,40 @@
+<?php 
+// output all html markup before <body>
+class BeginPage {
+	private $doctype = "<!doctype html>\n";
+	private $lang = "no";
+	private $title = "Default title";
+	private $level = 0;
+	private $cssbasepath = "style/style.css";
+	private $csspath = "";
+	private $script;
+	private $pageStartString;
+
+	private function buildPageStartString(){
+		$this->pageStartString = <<<EOT
+<!doctype html>
+<html lang="$this->lang">
+   <head>
+      <title>$this->title</title>
+      <meta charset="utf-8"/> 
+      <link rel="stylesheet" href="$this->csspath"/>
+      $this->script
+   </head>
+   <body>
+
+EOT;
+	}
+
+	public function display(){
+		if ($this->level > 0) {
+			for($i=0; $i < $this->level; $i++){
+				$this->csspath .= "../";
+			}
+			$this->csspath .= $this->cssbasepath;
+		} else {
+			$this->csspath = $this->cssbasepath;
+		}
+		$this->buildPageStartString();
+		return $this->pageStartString;
+	}
+}
